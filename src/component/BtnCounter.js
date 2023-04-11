584bece49f627da333ffbd01e7bc2bb0a9afb352
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2023-04-11 14:17:41 +0530

    completed

--- src/component/BtnCounter.js
+++ src/component/BtnCounter.js
@@ -3,18 +3,42 @@ import "./btnCounter.css";
 
 const BtnCounter = () => {
   const [counter, setCounter] = useState(1);
+  const [buttonColor, setButtonColor] = useState("red");
+  const [circleColor, setCircleColor] = useState("rgba(255, 214, 11, 0.954)");
 
-  const handleCounter = () => {
+  const handleCounter = (e, btnStyle) => {
     setCounter(counter + 1);
+    setCircleColor(buttonColor);
   };
 
   const formattedCounter = counter.toString().padStart(2, "0");
 
+  const btnStyle = {
+    backgroundColor: buttonColor,
+    border: "1px solid black",
+    fontSize: ".6rem",
+    cursor: "pointer",
+    padding: ".2rem .5rem",
+  };
+
+  const circleStyle = {
+    backgroundColor: circleColor,
+    borderRadius: "50%",
+    height: "9rem",
+    width: "9rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  };
+
   return (
     <div className="container">
-      <div className="circle">
+      <div style={circleStyle}>
         <h2>{formattedCounter}</h2>
-        <button onClick={handleCounter}>click to increase counter</button>
+        <button onClick={(e) => handleCounter(e, btnStyle)} style={btnStyle}>
+          click to increase counter
+        </button>
       </div>
     </div>
   );
